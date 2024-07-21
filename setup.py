b91dc49a27856e37cca0b5dcc9308c38e6e1b92c
chinmay396-indian <86561691+chinmay396-indian@users.noreply.github.com>
2024-07-21 21:01:39 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuputils import setup, find_packages
+
+setup(
+    name = "ECommerce App",
+    version = 0.1.0,
+    author = "Chinmay Sanjay Raut",
+    author_email = "r.chinmay396@gmail.com",
+    description = "This is an LLM powered ECommerce ChatBot that will answer all your queries regarding the desired product in a natural language",
+    url = "https://github.com/chinmay396-indian/ECommerce-Bot",
+    packages = find_packages(),
+)
